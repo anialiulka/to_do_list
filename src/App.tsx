473b782c3d16304c9cb32181089dc11293089b28
ania.liulka <anialiulka@gmail.com>
2023-11-03 17:07:36 -0400

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { NavLink, Route, Routes } from 'react-router-dom';
+import './App.scss';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink to="/">Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/tasks">Tasks</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/dashboard">Dashboard</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/settings">Settings</NavLink>
+                    </li>
+                </ul>
+            </nav>
+            <Routes>
+                <Route path="/" element={<div>Home</div>} />
+                <Route path="/tasks" element={<div>Tasks</div>} />
+                <Route path="/dashboard" element={<div>Dashboard</div>} />
+                <Route path="/settings" element={<div>Settings</div>} />
+            </Routes>
+        </>
+    );
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/to_do_list',
+    plugins: [react()],
+    resolve: { alias: { src: '/src' } },
+    base: '/to_do_list',
 });
